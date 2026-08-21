80a8d4dc4ec971a9688012bf9bcfdd38d582551b
Stanley Wang <stanley.wang74@proton.me>
2026-08-21 11:21:39 -0500

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.mins:
+            self.mins.append(val)
+        else:
+            self.mins.append(min(val, self.mins[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.mins.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.mins[-1]
