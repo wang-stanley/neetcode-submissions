3e883e60b867bb6ffb99b86108aa8b9cc54ccb4d
Stanley Wang <stanley.wang74@proton.me>
2026-08-21 15:39:26 -0500

    Add: largest-rectangle-in-histogram - submission-2

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # leftWalls[i] = index of the next shortest height to the left of index i
+        # rightWalls[i] = index of the next shortest height to the right of index i
+        leftWalls, rightWalls = [-1] * len(heights), [len(heights)] * len(heights)
+
+        missingWalls = []
+
+        for i, h in enumerate(heights):
+            while missingWalls and heights[missingWalls[-1]] > h:
+                rightWalls[missingWalls.pop()] = i
+
+            missingWalls.append(i)
+            # [ 0(7)  ]
+
+        missingWalls.clear()
+        for i in range(len(heights) - 1, -1, -1):
+            h = heights[i]
+            while missingWalls and heights[missingWalls[-1]] > h:
+                leftWalls[missingWalls.pop()] = i
+            
+            missingWalls.append(i)
+            
+        maxArea = 0
+
+        for i, h in enumerate(heights):
+            curArea = h * (rightWalls[i] - leftWalls[i] - 1)
+            maxArea = max(maxArea, curArea)
+
+        return maxArea
